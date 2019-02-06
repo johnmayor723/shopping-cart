a22bab88a0504bbd3f13d11f0e93e370ace870f2
John mayowa andrews <johnmayor723@yahoo.com>
2019-02-06 16:52:12 +0000

    commit4

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var Cart = require('../models/cart');
+
+
+router.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,4 +22,22 @@ router.get('/', function(req, res, next) {
   });
 });
 
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart.items : {});
+
+    Product.findById(productId, function (err, product) {
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        res.redirect('/');
+    });
+});
+router.get('/shopping-cart', function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
+    }
+    var cart = new Cart(req.session.cart.items);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
 module.exports = router;
